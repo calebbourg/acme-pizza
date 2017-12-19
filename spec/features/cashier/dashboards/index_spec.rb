076e5add8653fd7e84432ac0813857fd9abf05d0
ADMIN <calebbourg2@gmail.com>
2017-12-19 14:01:58 -0800

    feature specs for cashier/dashboards

---
+++ spec/features/cashier/dashboards/index_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'dashboards index', type: :feature do
+  let(:user) { create(:user) }
+
+  it 'shows pending order' do
+
+  end
+
+  it 'shows orders in process' do
+  end
+
+  it 'shows completed orders ready for pickup' do
+
+  end
+end
